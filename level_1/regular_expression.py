c14efa4ad6768ce8282f62f4794b34c924f6ec05
seunghun <xldi28@ajou.ac.kr>
2021-12-27 04:29:07 +0900

    21.12.26

--- level_1/regular_expression.py
+++ level_1/regular_expression.py
@@ -3,8 +3,13 @@ import re
 def solution(new_id):
     
     new_id = new_id.lower()
-    new_id = re.sub('[^a-z]', '', new_id)
-
+    new_id = re.sub('[^a-z0-9\-_.]', '', new_id)
+    new_id = re.sub('[.]+', '.', new_id)
+    print(new_id)
+    # 맨 앞에 오는 문자 ^., 맨 뒤에 오는 문자 .$
+    new_id = re.sub('^[.]|[.]$', '', new_id)
+    new_id = 'a' if len(new_id) == 0 else new_id[:15]
+    new_id = re.sub('[.]$', '', new_id)
+    while(len(new_id) < 3):
+        new_id += new_id[-1]
     return new_id
-
-print(solution('ABC123'))
